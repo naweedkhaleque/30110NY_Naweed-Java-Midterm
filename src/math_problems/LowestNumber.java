cf8ba9c759ddf89096a33a2fbabf0f5b0078fdf4
naweedkhaleque <nkhaleque17@gmail.com>
2022-10-15 21:42:03 -0400

    LowestNumber

--- src/math_problems/LowestNumber.java
+++ src/math_problems/LowestNumber.java
@@ -1,5 +1,7 @@
 package math_problems;
 
+import java.util.Arrays;
+
 public class LowestNumber {
 
     /** INSTRUCTIONS
@@ -11,7 +13,15 @@ public class LowestNumber {
         int[] array = new int[] {211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123,
                 67, 5, 679, 54, 32, 65};
 
+        System.out.println(getLowestNum(array));
 
+    }
+
+    public static int getLowestNum(int[] array) {
+        //sorting using Arrays class
+        Arrays.sort(array);
+        //returning first element of sorted int array (smallest value)
+        return array[0];
     }
 
 }
--- src/math_problems/UnitTestingMath.java
+++ src/math_problems/UnitTestingMath.java
@@ -18,8 +18,12 @@ public class UnitTestingMath {
 //        int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19, -16};
 //        FindLowestDifference.lowestDifferenceNumber(array1, array2);
 
-        int[] array3 = {10, 2, 1, 4, 5, 3, 7, 8, 6};
-        System.out.println(FindMissingNumber.missingNumber(array3));
+//        int[] array3 = {10, 2, 1, 4, 5, 3, 7, 8, 6};
+//        System.out.println(FindMissingNumber.missingNumber(array3));
+
+        int[] array4 = {211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123,
+                67, 5, 679, 54, 32, 65};
+        System.out.println(LowestNumber.getLowestNum(array4));
 
 
     }
