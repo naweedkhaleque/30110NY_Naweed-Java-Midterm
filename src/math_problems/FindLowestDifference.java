5a57b02da7bcab220da0314827cbc09071bb72d6
naweedkhaleque <nkhaleque17@gmail.com>
2022-10-15 20:33:42 -0400

    FindLowestDifference

--- src/math_problems/FindLowestDifference.java
+++ src/math_problems/FindLowestDifference.java
@@ -1,8 +1,14 @@
 package math_problems;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class FindLowestDifference {
 
-    /** INSTRUCTIONS
+    /**
+     * INSTRUCTIONS
      * Write a method to return the lowest number, that is not shared between the 2 arrays below
      * HINT: The lowest number that isn't shared between these arrays is 1
      */
@@ -12,9 +18,26 @@ public class FindLowestDifference {
 
         int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1, -15};
         int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19, -15};
-
+        lowestDifferenceNumber(array1, array2);
     }
 
+    public static void lowestDifferenceNumber(int[] arr1, int[] arr2) {
 
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
 
+        List<Integer> myList = new ArrayList<>();
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                myList.add(arr1[i]);
+            }
+            for (int j = 0; j < arr2.length; j++) {
+                if (arr1[j] != arr2[j]) {
+                    myList.add(arr2[j]);
+                }
+            }
+        }
+        Collections.sort(myList);
+        System.out.println(myList.get(0));
+    }
 }
--- src/math_problems/UnitTestingMath.java
+++ src/math_problems/UnitTestingMath.java
@@ -12,6 +12,10 @@ public class UnitTestingMath {
 //        //recursion
 //        System.out.println(Factorial.recursion(9));
 
-        Fibonacci.getFibonacci(40);
+        //Fibonacci.getFibonacci(40);
+
+        int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1, -15};
+        int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19, -16};
+        FindLowestDifference.lowestDifferenceNumber(array1, array2);
     }
 }
