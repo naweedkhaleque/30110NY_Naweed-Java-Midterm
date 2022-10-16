f2622ba30e2c4d0a4f3caff5451ddf8b64bdc560
naweedkhaleque <nkhaleque17@gmail.com>
2022-10-16 00:51:33 -0400

    UseMap

--- src/data_structures/UseMap.java
+++ src/data_structures/UseMap.java
@@ -35,6 +35,16 @@ public class UseMap {
         SharedStepsDatabase sql = new SharedStepsDatabase();
 
         // IMPLEMENT HERE
+        sql.insertMap(tableName, map);
 
+        String Query = "SELECT * FROM test_hash_map";
+        List<List<String>> results = sql.executeQueryReadAll(Query);
+
+        for (List<String> row : results) {
+            for (String cell : row) {
+                System.out.print(cell + "\t\t");
+            }
+            System.out.println();
+        }
     }
 }
