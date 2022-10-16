33b881064c4e1c9c91403e80c874728aa415a2dd
naweedkhaleque <nkhaleque17@gmail.com>
2022-10-16 00:42:01 -0400

    array list

--- src/data_structures/UseArrayList.java
+++ src/data_structures/UseArrayList.java
@@ -1,5 +1,12 @@
 package data_structures;
 
+import databases.SharedStepsDatabase;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class UseArrayList {
 
     /** INSTRUCTIONS
@@ -10,7 +17,46 @@ public class UseArrayList {
      * Store and retrieve the data from/to a database table.
      */
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
+        List<Object> someWords = new ArrayList<>();
+
+        someWords.add("These");
+        someWords.add("are");
+        someWords.add("some");
+        someWords.add("words!");
+        System.out.println(someWords);
+
+        someWords.add("Another one");
+        System.out.println(someWords);
+
+        someWords.remove("Another one");
+        System.out.println(someWords);
+
+        for (Object words : someWords) {
+            System.out.println(words);
+        }
+
+        Iterator<Object> myIterator1 = someWords.iterator();
+        while (myIterator1.hasNext()) {
+            System.out.println(myIterator1.next());
+        }
+
+
+        SharedStepsDatabase ssdb = new SharedStepsDatabase();
+
+        //inserting to database
+        ssdb.insertList("words_for_ArrayList", "AL_column", someWords);
+
+        //querying to database
+        String Query = "SELECT * FROM words_for_ArrayList";
+
+        //retrieving from the database
+        List<String> results = ssdb.executeQueryReadAllSingleColumn(Query, "AL_column");
+
+        for (String s: results) {
+            System.out.println(s);
+        }
 
     }
 
