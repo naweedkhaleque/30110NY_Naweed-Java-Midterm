451746050c97c1759e67eff70cb753821b4ef402
naweedkhaleque <nkhaleque17@gmail.com>
2022-10-16 00:12:13 -0400

    DataReader

--- src/data_structures/DataReader.java
+++ src/data_structures/DataReader.java
@@ -1,5 +1,11 @@
 package data_structures;
 
+import java.io.*;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class DataReader {
 
     /** INSTRUCTIONS
@@ -19,8 +25,61 @@ public class DataReader {
      **/
 
     public static void main(String[] args) {
-        String textFilePath = System.getProperty("user.dir") + "/src/data_structures/data/self-driving-car.txt";
+        //fixed file path
+        String textFilePath = System.getProperty("user.dir") + File.separator + "src" + File.separator +
+                "data_structures" + File.separator + "data" + File.separator + "self-driving-car";
 
+        //create new file
+        File file = new File(textFilePath);
+
+        //try-catch with exceptions and BufferedReader
+        try (FileInputStream fis = new FileInputStream(file);
+             InputStreamReader isr = new InputStreamReader(fis);
+             BufferedReader br = new BufferedReader(isr)){
+
+            String line;
+
+            LinkedList<String> myLinkedList = new LinkedList<>();
+            Stack<String> myStack = new Stack<>();
+
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                String[] splitSentence = line.split(" ");
+                myLinkedList.add(Arrays.toString(splitSentence));
+                myStack.add(Arrays.toString(splitSentence));
+            }
+
+            Iterator<String> LinkedListIterator = myLinkedList.iterator();
+            while (LinkedListIterator.hasNext()) {
+                System.out.println(LinkedListIterator.next());
+            }
+
+            Iterator<String> StackIterator = myStack.iterator();
+            while (StackIterator.hasNext()) {
+                System.out.println(StackIterator.next());
+            }
+
+            myStack.add("Some Text added with Stack");
+            myStack.add("Another text");
+            myStack.remove("Some Text added with Stack");
+
+            for (String string : myStack) {
+                System.out.println(string);
+            }
+            myStack.pop();
+            myStack.push("something to push");
+
+            for (String string : myStack) {
+                System.out.println(string);
+            }
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("NO FILE EXISTS AT THE PROVIDED PATH!");
+        } catch (IOException e) {
+            System.out.println("COULD NOT READ FROM FILE. PLEASE CHECK YOUR FILE IS NOT CORRUPTED");
+        }
     }
-
 }
+
+
