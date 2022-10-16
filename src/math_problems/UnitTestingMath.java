0dd1767fc3e9b1547a7af93fb76d0e1a2b1832a7
naweedkhaleque <nkhaleque17@gmail.com>
2022-10-15 23:35:00 -0400

    Collection view printed

--- src/math_problems/UnitTestingMath.java
+++ src/math_problems/UnitTestingMath.java
@@ -21,9 +21,9 @@ public class UnitTestingMath {
 //        int[] array3 = {10, 2, 1, 4, 5, 3, 7, 8, 6};
 //        System.out.println(FindMissingNumber.missingNumber(array3));
 
-        int[] array4 = {211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123,
-                67, 5, 679, 54, 32, 65};
-        System.out.println(LowestNumber.getLowestNum(array4));
+//        int[] array4 = {211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123,
+//                67, 5, 679, 54, 32, 65};
+//        System.out.println(LowestNumber.getLowestNum(array4));
 
 
     }
