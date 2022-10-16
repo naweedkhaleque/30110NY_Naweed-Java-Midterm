ef3e2b81b6ab27361bda67826198b1d66794ea2b
naweedkhaleque <nkhaleque17@gmail.com>
2022-10-15 21:32:38 -0400

    FindMissingNumber

--- src/math_problems/FindMissingNumber.java
+++ src/math_problems/FindMissingNumber.java
@@ -8,6 +8,15 @@ public class FindMissingNumber {
 
     public static void main(String[] args) {
         int[] array = new int[] {10, 2, 1, 4, 5, 3, 7, 8, 6};
+        System.out.println(missingNumber(array));
+    }
 
+    public static int missingNumber(int[] myArray) {
+        // sum total = N * (N+1) / 2
+        int sum = ((myArray.length + 1) * (myArray.length + 2)) / 2;
+        //iterate through with for-loop and decrease the value of sum by the values in array throughout loop
+        for (int i = 0; i < myArray.length; i++)
+            sum -= myArray[i];
+        return sum;
     }
 }
--- src/math_problems/UnitTestingMath.java
+++ src/math_problems/UnitTestingMath.java
@@ -14,8 +14,13 @@ public class UnitTestingMath {
 
         //Fibonacci.getFibonacci(40);
 
-        int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1, -15};
-        int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19, -16};
-        FindLowestDifference.lowestDifferenceNumber(array1, array2);
+//        int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1, -15};
+//        int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19, -16};
+//        FindLowestDifference.lowestDifferenceNumber(array1, array2);
+
+        int[] array3 = {10, 2, 1, 4, 5, 3, 7, 8, 6};
+        System.out.println(FindMissingNumber.missingNumber(array3));
+
+
     }
 }
