603fbdf5043df1589883740fc1fe7fb56d240a95
naweedkhaleque <nkhaleque17@gmail.com>
2022-10-15 23:15:33 -0400

    prime numbers

--- src/math_problems/PrimeNumber.java
+++ src/math_problems/PrimeNumber.java
@@ -11,7 +11,31 @@ public class PrimeNumber {
 
 
     public static void main(String[] args) {
-
+        getPrimeNumber(2, 1_000_000);
     }
 
+    public static void getPrimeNumber(int a, int b) {
+        // an int variable flag to keep track if i is prime number or not
+        // 0 = not, 1 = is prime
+        int flag;
+
+        for (int i = a; i <= b; i++) {
+            // 1 and 0 not prime numbers
+            if (i == 1 || i == 0) {
+                continue;
+            }
+            flag = 1;
+
+            for (int j = 2; j <=  i/2; j++) {
+                if (i % j == 0) {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag == 1)
+                System.out.println(i);
+        }
+    }
 }
+
+
